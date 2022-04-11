e269d0b6930f27de5b736adf8c9556851593a8d8
Muhammad Fadil <fadiljayaputra@gmail.com>
2022-04-11 13:20:26 +0700

    did the page 2

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const page2 = document.getElementById("page2")
 cta.addEventListener("click", () => {
     cta.classList.add("hidden");
     header1.classList.add("hidden");
-    page2.classList.remove("hidden")
+    page2.classList.add("display")
 })
 
 const loaded = () => {
     cta.classList.remove("hidden");
     header1.classList.remove("hidden");
-    page2.classList.add("hidden");
+    page2.classList.remove("display");
 }
