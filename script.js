2c2ef7c2eaa9bacc5817597bb2fb50a5a12104d9
Muhammad Fadil <fadiljayaputra@gmail.com>
2022-04-12 09:47:56 +0700

    LETSGO, Made the overlay using style.display intstead of adding a class to the overlay

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const page2 = document.getElementById("page2")
 const overlay = document.getElementById("pemenang")
 const backButton = document.getElementById("back")
 const cards = document.getElementsByClassName("cards")
+const overlays = {
+    akustik: document.getElementById('akustik'),
+    cci: document.getElementById('akustik'),
+    cipcer: document.getElementById('akustik'),
+    dai: document.getElementById('akustik'),
+    fashion: document.getElementById('akustik'),
+    kaligrafi: document.getElementById('akustik'),
+    mhq: document.getElementById('akustik'),
+    mtq: document.getElementById('akustik'),
+    poster: document.getElementById('akustik'),
+    puisi: document.getElementById('akustik')
+}
 
 cta.addEventListener("click", () => {
     cta.classList.add("hidden");
     header1.classList.add("hidden");
     page2.classList.add("display");
 })
-
-backButton.addEventListener("click", back())
 
 function loaded() {
     cta.classList.remove("hidden");
@@ -22,8 +32,12 @@ function loaded() {
 
 function clicked() {
     page2.classList.add('clicked');
+    backButton.classList.add('appear');
+    overlays.akustik.style.display = "flex"
 }
 
 function back() {
-    page2.classList.remove('clicked')
+    page2.classList.remove('clicked');
+    overlays.akustik.style.display = "none";
+    backButton.classList.remove('appear')
 }
