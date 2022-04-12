78b97452d2cdcd798c261ddf3b486f7bd7553ba9
Muhammad Fadil <fadiljayaputra@gmail.com>
2022-04-12 07:11:11 +0700

    added board buat daftar pemenang

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const cta = document.getElementById("mascot")
 const header1 = document.getElementById("header1")
 const page2 = document.getElementById("page2")
+const overlay = document.getElementById("pemenang")
 
 cta.addEventListener("click", () => {
     cta.classList.add("hidden");
     header1.classList.add("hidden");
-    page2.classList.add("display")
+    page2.classList.add("display");
 })
 
 const loaded = () => {
     cta.classList.remove("hidden");
     header1.classList.remove("hidden");
     page2.classList.remove("display");
+    overlay.classList.remove("display")
 }
