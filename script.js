b536d5f6f12e449a211fe0757548a4501ac709ce
Muhammad Fadil <fadiljayaputra@gmail.com>
2022-04-12 08:29:54 +0700

    added functionality on board, added back button, added .gitignore file

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const cta = document.getElementById("mascot")
 const header1 = document.getElementById("header1")
 const page2 = document.getElementById("page2")
 const overlay = document.getElementById("pemenang")
+const backButton = document.getElementById("back")
+const cards = document.getElementsByClassName("cards")
 
 cta.addEventListener("click", () => {
     cta.classList.add("hidden");
@@ -9,9 +11,19 @@ cta.addEventListener("click", () => {
     page2.classList.add("display");
 })
 
-const loaded = () => {
+backButton.addEventListener("click", back())
+
+function loaded() {
     cta.classList.remove("hidden");
     header1.classList.remove("hidden");
-    page2.classList.remove("display");
+    page2.classList.remove("display clicked");
     overlay.classList.remove("display")
+}
+
+function clicked() {
+    page2.classList.add('clicked');
+}
+
+function back() {
+    page2.classList.remove('clicked')
 }
